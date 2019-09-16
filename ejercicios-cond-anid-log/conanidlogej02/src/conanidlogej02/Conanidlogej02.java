2074d173bf498e4d49c3d31b3da9d5747572cd6f
Toyama Rodrigo <toyama.rodrigo@gmail.com>
2019-09-16 17:57:12 -0300

    added condicional anidado logico ej 02

--- ejercicios-cond-anid-log/conanidlogej02/src/conanidlogej02/Conanidlogej02.java
+++ ejercicios-cond-anid-log/conanidlogej02/src/conanidlogej02/Conanidlogej02.java
@@ -1,11 +1,28 @@
 package conanidlogej02;
 
+import javax.swing.JOptionPane;
+
 public class Conanidlogej02 {
 
     public static void main(String[] args) {
 
-        
-        
+        /*
+        Dados 3 números determinar cual es el mayor
+         */
+        int A, B, C;
+
+        A = Integer.parseInt(JOptionPane.showInputDialog("Ingrese valor A: "));
+        B = Integer.parseInt(JOptionPane.showInputDialog("Ingrese valor B: "));
+        C = Integer.parseInt(JOptionPane.showInputDialog("Ingrese valor C: "));
+
+        if ( A >= B && A >= C ){
+            JOptionPane.showMessageDialog(null, "A es el mayor: "+A);
+        } else if ( B >= A && B >= C ) {
+            JOptionPane.showMessageDialog(null, "B es el mayor: "+B);
+        } else {
+            JOptionPane.showMessageDialog(null, "C es el mayor: "+C);
+        }
+
     }
-    
+
 }
