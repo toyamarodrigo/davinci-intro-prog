8e588e2523c1f80d8f844c43a865a69efbc9ea2e
Toyama Rodrigo <toyama.rodrigo@gmail.com>
2019-10-07 17:48:28 -0300

    modify selector multiple 03

--- ejercicios-selector-mult/selectormult03/src/selectormult03/Selectormult03.java
+++ ejercicios-selector-mult/selectormult03/src/selectormult03/Selectormult03.java
@@ -4,9 +4,9 @@ import javax.swing.JOptionPane;
 
 public class Selectormult03 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		/*
+        /*
 		 * Calcular el sueldo de un operario teniendo en cuenta la cantida de horas
 		 * extras y la cant de horas, de acuerdo a los valores indicados
 		 * 
@@ -22,21 +22,30 @@ public class Selectormult03 {
 		 * le paga un 7% menos del valor indicado en la tabla a cada categoria.
 		 * 
 		 * */
-		
-		int cat, hs_trabajo;
-		cat = Integer.parseInt(JOptionPane.showInputDialog("Ingrese Categoria: \n1 - 2 - 3 "));
-		hs_trabajo = Integer.parseInt(JOptionPane.showInputDialog("Ingrese Horas Trabajadas: "));
-		
-		switch(cat) {
-		case 1:
-			
-			break;
-		case 2:
-			break;
-		case 3:
-			break;
-		}
-		
-	}
+        int cat, hs_trabajo, hs_extra, toths;
+        double preciohs, precioextra, totprecio;
+        cat = Integer.parseInt(JOptionPane.showInputDialog("Ingrese Categoria: \n1 - 2 - 3 "));
+        hs_trabajo = Integer.parseInt(JOptionPane.showInputDialog("Ingrese Horas Trabajadas: "));
+        hs_extra = Integer.parseInt(JOptionPane.showInputDialog("Ingrese Horas Extra: "));
+
+        switch (cat) {
+            case 1:
+                preciohs = hs_trabajo * 14;
+                precioextra = hs_extra * 20.50; 
+                totprecio = preciohs + precioextra;
+                if (hs_extra > 30) {
+                    totprecio -= totprecio * 0.07;
+                } else {
+                    
+                }
+
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
+
+    }
 
 }
