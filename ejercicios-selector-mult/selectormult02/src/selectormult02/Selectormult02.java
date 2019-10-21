daf6e26a8f09248cbc407d46913d3144bbe0d6b2
Toyama Rodrigo <toyama.rodrigo@gmail.com>
2019-10-21 17:31:10 -0300

    added selector multiple ej 05 hecho, 2 opciones

---
+++ ejercicios-selector-mult/selectormult02/src/selectormult02/Selectormult02.java
@@ -0,0 +1,59 @@
+package selectormult02;
+
+import javax.swing.JOptionPane;
+
+public class Selectormult02 {
+
+    public static void main(String[] args) {
+
+        /*
+	* El costo de las llamadas internacionales depende de la zona geografica del
+	* pais destino y de la cantidad de minutos hablados. En la siguiente tabla se
+	* representa el costo del minuto por zona, cada zona tiene un numero clave
+	* conocido por el operador y si el costo supera los $15, se realiza un 15% de
+	* descuento, solo se ingresa la clave y los minutos. El programa debe indicar
+	* la zona y el total a pagar.
+	* 
+	* Clave 		Zona 				Precio 
+	* 12 			America del norte 		0.68 
+	* 15 			America Central 		0.55 
+	* 18 			AMerica sur 			0.62 
+	* 19 			Europa 				0.85 
+	* 23 			Asia 				0.89 
+	* 25 			Africa 				0.82
+         */
+        double tot = 0, minutos;
+        int clave;
+        clave = Integer.parseInt(JOptionPane.showInputDialog("Ingrese Clave"));
+        minutos = Double.parseDouble(JOptionPane.showInputDialog("Ingrese Minutos"));
+
+        switch (clave) {
+            case 12:
+                tot = minutos * 0.68;
+                break;
+            case 15:
+                tot = minutos * 0.55;
+                break;
+            case 18:
+                tot = minutos * 0.62;
+                break;
+            case 19:
+                tot = minutos * 0.85;
+                break;
+            case 23:
+                tot = minutos * 0.89;
+                break;
+            case 25:
+                tot = minutos * 0.82;
+                break;
+        }
+
+        if (tot > 15) {
+            tot -= tot * 0.15;
+            JOptionPane.showMessageDialog(null, "Se aplica descuento 15%, Total: " + tot);
+        } else {
+            JOptionPane.showMessageDialog(null, "Total: " + tot);
+        }
+    }
+
+}
--- ejercicios-selector-mult/selectormult05/src/selectormult05/Selectormult05.java
+++ ejercicios-selector-mult/selectormult05/src/selectormult05/Selectormult05.java
@@ -1,11 +1,17 @@
 package selectormult05;
 
+import javax.swing.JOptionPane;
+
 public class Selectormult05 {
 
-public static void main(String[] args) throws Exception {
-    
+    public static void main(String[] args) throws Exception {
+
         /*
-        Una empresa utiliza el siguiente tipo de comercializacion para sus productos. Se tienen 2 productos A y B, el producto A tiene tres tipos de fragancias diferentes (primavera, marino y otoño) y el proucto B tiene tres tipos de presentaciones diferentes (pequeño, mediano y grande)
+        Una empresa utiliza el siguiente tipo de comercializacion para sus 
+        productos. Se tienen 2 productos A y B, el producto A tiene tres tipos 
+        de fragancias diferentes (primavera, marino y otoño) y el proucto B 
+        tiene tres tipos de presentaciones diferentes (pequeño, mediano y 
+        grande)
         COSTO
         PRODUCTO A
         Otoño       $1.50
@@ -17,9 +23,164 @@ public static void main(String[] args) throws Exception {
         mediano     $1.60
         pequeño     $1.10
 
-        Indicar valor de la compra, si solo se puede comprar un solo tipo de producto y si la cantidad comprada supera los $100, se debe realizar un descuento del 10% sobre el total de la compra
-        */
+        Indicar valor de la compra, si solo se puede comprar un solo tipo de 
+        producto y si la cantidad comprada supera los $100, se debe realizar 
+        un descuento del 10% sobre el total de la compra
+         */
+        int cant, fragancia, tamano;
+        double tot;
+
+        cant = Integer.parseInt(JOptionPane.showInputDialog("Ingrese cantidad: "));
+
+        /*
+        fragancia = Integer.parseInt(JOptionPane.showInputDialog("Ingrese tipo de fragancia: "
+                + "\n1) Otoño"
+                + "\n2) Primavera"
+                + "\n3) Marino"));
+        tamano = Integer.parseInt(JOptionPane.showInputDialog("Ingrese tamaño: "
+                + "\n1) Grande"
+                + "\n2) Mediano"
+                + "\n3) Chico"));
+
+        switch (fragancia) {
+            case 1:
+                switch (tamano) {
+                    case 1:
+                        tot = 1.50 * 2.05 * cant;
+                        JOptionPane.showConfirmDialog(null, "total: " + tot);
+                        break;
+                    case 2:
+                        tot = 1.50 * 1.60 * cant;
+                        JOptionPane.showConfirmDialog(null, "total: " + tot);
+                        break;
+                    case 3:
+                        tot = 1.50 * 1.10 * cant;
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(null, "tamaño incorrecto");
+                        break;
+                }
+                break;
+            case 2:
+                switch (tamano) {
+                    case 1:
+                        tot = 1.50 * 2.05 * cant;
+                        JOptionPane.showConfirmDialog(null, "total: " + tot);
+                        break;
+                    case 2:
+                        tot = 1.55 * 1.60 * cant;
+                        JOptionPane.showConfirmDialog(null, "total: " + tot);
+                        break;
+                    case 3:
+                        tot = 1.60 * 1.10 * cant;
+                        JOptionPane.showConfirmDialog(null, "total: " + tot);
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(null, "tamaño incorrecto");
+                        break;
+                }
+                break;
+            case 3:
+                switch (tamano) {
+                    case 1:
+                        tot = 1.50 * 2.05 * cant;
+                        JOptionPane.showConfirmDialog(null, "total: " + tot);
+                        break;
+                    case 2:
+                        tot = 1.55 * 1.60 * cant;
+                        JOptionPane.showConfirmDialog(null, "total: " + tot);
+                        break;
+                    case 3:
+                        tot = 1.60 * 1.10 * cant;
+                        JOptionPane.showConfirmDialog(null, "total: " + tot);
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(null, "tamaño incorrecto");
+                        break;
+                }
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Fragancia incorrecta");
+        }
+         */
+        int producto, tipoa, tipob;
+
+        producto = Integer.parseInt(JOptionPane.showInputDialog("Ingrese tipo de producto "
+                + "\n1) A"
+                + "\n2) B"));
+
+        tipoa = Integer.parseInt(JOptionPane.showInputDialog("Ingrese tipo de fragancia: "
+                + "\n1) Otoño"
+                + "\n2) Primavera"
+                + "\n3) Marino"));
+
+        tipob = Integer.parseInt(JOptionPane.showInputDialog("Ingrese tipo de presentacion: "
+                + "\n1) Grande"
+                + "\n2) Mediano"
+                + "\n3) Chico"));
+        
+        switch (producto) {
+            case 1:
+                switch (tipoa) {
+                    case 1:
+                        tot = 1.50 * cant;
+                        
+                        if(tot > 100) { tot -= tot*0.10; } 
+                        
+                        JOptionPane.showMessageDialog(null, "Total: " + tot);
+                        break;
+                    case 2:
+                        tot = 1.55 * cant;
+                        
+                        if(tot > 100) { tot -= tot*0.10; } 
+                        
+                        JOptionPane.showMessageDialog(null, "Total: " + tot);
+                        break;
+                    case 3:
+                        tot = 1.60 * cant;
+                        
+                        if(tot > 100) { tot -= tot*0.10; } 
+                        
+                        JOptionPane.showMessageDialog(null, "Total: " + tot);
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(null, "Error tipo de productoA");
+                        break;
+                }
+                break;
+            case 2:
+                switch (tipob) {
+                    case 1:
+                        tot = 2.05 * cant;
+                        
+                        if(tot > 100) { tot -= tot*0.10; } 
+                        
+                        JOptionPane.showMessageDialog(null, "Total: " + tot);
+                        break;
+                    case 2:
+                        tot = 1.60 * cant;
+                        
+                        if(tot > 100) { tot -= tot*0.10; } 
+                        
+                        JOptionPane.showMessageDialog(null, "Total: " + tot);
+                        break;
+                    case 3:
+                        tot = 1.10 * cant;
+                        
+                        if(tot > 100) { tot -= tot*0.10; } 
+                        
+                        JOptionPane.showMessageDialog(null, "Total: " + tot);
+                        break;
+                    default:
+                        JOptionPane.showMessageDialog(null, "Error tipo de productoB");
+                        break;
+                }
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Error tipo de producto");
+                break;
+        }
 
     }
-    
+
 }
