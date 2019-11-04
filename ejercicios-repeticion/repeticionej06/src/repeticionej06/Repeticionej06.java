95a2ae30b5afbcf85bd4987f9d88d311861ffc38
Toyama Rodrigo <toyama.rodrigo@gmail.com>
2019-11-04 17:47:49 -0300

    added ejercicio repeticion 6

--- ejercicios-repeticion/repeticionej06/src/repeticionej06/Repeticionej06.java
+++ ejercicios-repeticion/repeticionej06/src/repeticionej06/Repeticionej06.java
@@ -11,23 +11,24 @@ public class Repeticionej06 {
         Dados n números enteros calcular el rango. Rango: es la diferencia 
         entre el numero mayor y el menor
          */
-        int num1, numeros, rangonum, temp, tot, nummay = Integer.MIN_VALUE, nummin = Integer.MAX_VALUE;
+        int num1, numero, rangonum, temp, tot, nummay = Integer.MIN_VALUE, nummin = Integer.MAX_VALUE;
         Random random = new Random();
 
         rangonum = Integer.parseInt(JOptionPane.showInputDialog("Ingrese max de numeros: "));
 
         for (int i = 0; i < rangonum; i++) {
-            numeros = random.nextInt(51);
+            numero = random.nextInt(51);
 
-            if (numeros > nummay) {
-                nummin = nummay;
-                nummay = numeros;
-            } else {
-                nummin = numeros;
+            if (numero > nummay) {
+                temp = nummay;
+                nummin = temp;
+                nummay = numero;
+            } else if (numero < nummin) {
+                nummin = numero;
             }
             
             System.out.println("Contador: " + i +
-                    "\nNumero random: " + numeros +
+                    "\nNumero random: " + numero +
                     "\nNumero mayor: " + nummay + 
                     "\nNumero menor: " + nummin);
             
