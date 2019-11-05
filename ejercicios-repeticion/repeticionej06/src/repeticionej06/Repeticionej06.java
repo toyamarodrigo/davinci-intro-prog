981ceb07c30af85de216484e7f07e4280d7ead4c
Toyama Rodrigo <toyama.rodrigo@gmail.com>
2019-11-05 18:49:28 -0300

    completado ejercicio repeticion 6

--- ejercicios-repeticion/repeticionej06/src/repeticionej06/Repeticionej06.java
+++ ejercicios-repeticion/repeticionej06/src/repeticionej06/Repeticionej06.java
@@ -5,41 +5,43 @@ import javax.swing.JOptionPane;
 
 public class Repeticionej06 {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        /*
-        Dados n números enteros calcular el rango. Rango: es la diferencia 
-        entre el numero mayor y el menor
-         */
-        int num1, numero, rangonum, temp, tot, nummay = Integer.MIN_VALUE, nummin = Integer.MAX_VALUE;
-        Random random = new Random();
+		/*
+		 * Dados n números enteros calcular el rango. Rango: es la diferencia entre el
+		 * numero mayor y el menor
+		 */
+		int numero, rangonum, tot, nummax = Integer.MIN_VALUE, nummin = Integer.MAX_VALUE;
+		Random random = new Random();
 
-        rangonum = Integer.parseInt(JOptionPane.showInputDialog("Ingrese max de numeros: "));
+		rangonum = Integer.parseInt(JOptionPane.showInputDialog("Ingrese max de numeros: "));
 
-        for (int i = 0; i < rangonum; i++) {
-            numero = random.nextInt(51);
+		for (int i = 0; i < rangonum; i++) {
+			numero = random.nextInt(51);
 
-            if (numero > nummay) {
-                temp = nummay;
-                nummin = temp;
-                nummay = numero;
-            } else if (numero < nummin) {
-                nummin = numero;
-            }
-            
-            System.out.println("Contador: " + i +
-                    "\nNumero random: " + numero +
-                    "\nNumero mayor: " + nummay + 
-                    "\nNumero menor: " + nummin);
-            
-        }
+			if (numero < nummin) {
+				nummin = numero;
+			}
 
-        tot = nummay - nummin;
+			if (numero > nummax) {
+				nummax = numero;
+			}
 
-        JOptionPane.showMessageDialog(null, "Numero mayor: " + nummay
-                + "\nNumero menor: " + nummin
-                + "\nDiferencia entre numeros: " + tot);
+			System.out.println("Contador: " + i 
+					+ "\nNumero random: " + numero 
+					+ "\nNumero mayor: " + nummax
+					+ "\nNumero menor: " + nummin
+					);
+		}
 
-    }
+		tot = nummax - nummin;
+
+		JOptionPane.showMessageDialog(null,
+				"Numero mayor: " + nummax 
+				+ "\nNumero menor: " + nummin 
+				+ "\nDiferencia entre numeros: " + tot
+				);
+
+	}
 
 }
