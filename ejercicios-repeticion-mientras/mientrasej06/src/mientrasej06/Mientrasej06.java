5b7eacd6f73bd7bcd998dd053464b3531e319721
Toyama Rodrigo <toyama.rodrigo@gmail.com>
2019-11-15 17:42:54 -0300

    added ejercicio mientras 6 y 7

--- ejercicios-repeticion-mientras/mientrasej06/src/mientrasej06/Mientrasej06.java
+++ ejercicios-repeticion-mientras/mientrasej06/src/mientrasej06/Mientrasej06.java
@@ -1,5 +1,7 @@
 package mientrasej06;
 
+import javax.swing.JOptionPane;
+
 public class Mientrasej06 {
 
     public static void main(String[] args) {
@@ -10,8 +12,9 @@ public class Mientrasej06 {
 
         Se pide imprimir: 
             a) total de alumnas relevadas
-            b) cantidatotal de alumnas relevadasd de alumnas que tienen 
-            aptitudes para formar el equipo: altura >= 1.73 y peso entre 55 y 83 kg
+            b) cantidatotal de alumnas relevadas de alumnas que tienen 
+            aptitudes para formar el equipo: altura >= 1.73 y peso entre 55 
+            y 83 kg
             c) porcentaje que representa la cantidad anterior con respecto al 
             total de alumnas relevadas
             d) total de alumnos varones relevadas
@@ -21,10 +24,54 @@ public class Mientrasej06 {
             total de alumnos varones relevados
             g) total de alumnos relevados
             Este programa finalizara con la pregunta ¿ingresa otro registro?
-        */
-        
-        
+         */
+        int alumnas = 0, alumnos = 0, totalum = 0, ralumnas = 0, ralumnos = 0;
+        double altura, peso, palumnas, palumnos, porcentaje, totralumnos;
+        String preg, sexo;
 
+        do {
+
+            sexo = JOptionPane.showInputDialog("(M)asculino o (F)emenino ?");
+            altura = Double.parseDouble(JOptionPane.showInputDialog("Ingrese altura: "));
+            peso = Double.parseDouble(JOptionPane.showInputDialog("Ingrese peso: "));
+
+            switch (sexo) {
+                case "f":
+                    alumnas++;
+                    totalum++;
+                    if (altura >= 1.73 && peso >= 55 && peso <= 83) {
+                        ralumnas++;
+                    }
+                    break;
+                case "m":
+                    alumnos++;
+                    totalum++;
+                    if (altura >= 1.83 && peso >= 73 && peso <= 105) {
+                        ralumnos++;
+                    }
+                    break;
+                default:
+                    System.out.println("Error sexo");
+                    break;
+            }
+            
+            System.out.println("Alumnos: "+alumnos);
+            System.out.println("Alumnas: "+alumnas);
+            System.out.println("Total: "+totalum);
+            System.out.println("Alumnos aptos: " +ralumnos);
+            System.out.println("Alumnas aptas: " +ralumnas);
+            
+            preg = JOptionPane.showInputDialog("¿ingresa otro registro?");
+
+        } while (preg.equalsIgnoreCase("y"));
+
+        palumnas = (ralumnas * 100) / alumnas;
+        System.out.println("Porcentaje alumnas relevadas: " +palumnas);
+        palumnos = (ralumnos * 100) / alumnos;
+        System.out.println("Porcentaje alumnos relevadas: " +palumnos);
+        
+        totralumnos = ralumnas + ralumnos;
+        System.out.println("Total de alumnos relevados: " +totralumnos);
     }
-    
+
 }
---
+++ ejercicios-repeticion-mientras/mientrasej07/src/mientrasej07a/Mientrasej07a.java
@@ -0,0 +1,29 @@
+package mientrasej07a;
+
+public class Mientrasej07a {
+
+    public static void main(String[] args) {
+
+        /*
+        Determinar la estructura de repetición y realizar
+ 
+            1) En una librería se realizan ventas de dos tipos: contado y 
+            tarjeta de crédito Se pide obtener la siguiente información 
+            correspondiente a un día de trabajo
+            a) cantidad de ventas con tarjeta
+            b) total de efectivo al realizar el arqueo de caja
+            c) monto total de ventas con tarjeta
+            d) monto total de ventas del día
+            e) venta máxima
+            f) nombre vendedor venta máxima
+        
+        Indicar cual seria la condición de fin para utilizar un for, do o 
+        while y realizar con una de ellas (do o while)
+
+        */
+        
+        
+
+    }
+    
+}
